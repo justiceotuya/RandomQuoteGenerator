38668cff296c6d1d0c429cf5effff5f893d8985c
Justice Otuya <jotuya2@gmail.com>
2018-07-19 08:01:48 +0100

    Changed api vendor

--- randomQuote.js
+++ randomQuote.js
@@ -1,36 +1,36 @@
 $("document").ready(function () {
-    
+
     getData();
     $('#getQuote').click(function () {
-            getData();
-        })
+        getData();
+    });
 
     //function to get data from api
     function getData() {
-        
-    $.ajax({
-      url: 'http://api.forismatic.com/api/1.0/',
-      jsonp: 'jsonp',
-      dataType: 'jsonp',
-      data: {
-        method: 'getQuote',
-        lang: 'en',
-        format: 'jsonp'
-      },
-        success: function (data) {
-       
+
+        $.ajax({
+            url: 'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp'
+            },
+            success: function (data) {
+                console.log(data);
                 var currentQuote = data.quoteText;
                 var currentAuthor = data.quoteAuthor;
                 $("#quotes").html(currentQuote);
                 $("#author").html(currentAuthor);
-                
+
             }
-        })
+        });
     }
 
     // Sharing the Tweet
     $("#tweet").on("click", function () {
-       window.open("https://twitter.com/intent/tweet?text=" + $('#quotes').text() + " - " + $('#author').text(), "_blank");
+        window.open("https://twitter.com/intent/tweet?text=" + $('#quotes').text() + " - " + $('#author').text(), "_blank");
     });
-    
+
 });
